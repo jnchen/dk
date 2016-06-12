d4f5be37be3145bb41f13ca91246f0b2a2561fc3
jnchen <caojingchen@live.com>
2016-06-12 14:42:37 +0800

    Signed-off-by: jnchen <caojingchen@live.com>

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -68,8 +68,8 @@ class User_model extends CI_Model {
 	public function login(){
 
 		$this->db->select('id,username,password');
-		$this->db->where('username',$username);
-		$this->db->where('password',md5($password));
+		$this->db->where('username',$this->input->post('username'));
+		$this->db->where('password',md5($this->input->post('password')));
 
 		$query = $this->db->get('users');
 
