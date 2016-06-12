4c9388f190c5047c7e98a09b2eae62a3b8254b0c
jnchen <caojingchen@live.com>
2016-06-12 17:24:36 +0800

    Signed-off-by: jnchen <caojingchen@live.com>

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,11 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Test extends JC_Controller {
+class Test extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 	}
 
 	public function index(){
-		echo 'Already login';
+		$this->load->model('User_model');
+
+		$this->User_model->is_login();
 	}
 }
