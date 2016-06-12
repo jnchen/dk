722c60a3b5d99964ba55a2580a99fc4ad2b3ce81
jnchen <caojingchen@live.com>
2016-06-12 18:06:24 +0800

    Signed-off-by: jnchen <caojingchen@live.com>

--- application/core/JC_Controller.php
+++ application/core/JC_Controller.php
@@ -16,8 +16,6 @@ class JC_Controller extends CI_Controller {
 			$this->input->set_cookie("username",$this->cur_user['username'],60);
 			$this->input->set_cookie('token',gen_token($this->cur_user['password']),60);
 			$this->input->set_cookie('id',$this->cur_user['id'],60);
-
-			header("location:".site_url("Test"));
 		}
 	}
 }
