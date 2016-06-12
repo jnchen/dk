96cc860d0fe6fc2d16fc1665cedd7bb4fb6366a0
jnchen <caojingchen@live.com>
2016-06-12 10:52:36 +0800

    Signed-off-by: jnchen <caojingchen@live.com>

--- application/config/database.php
+++ application/config/database.php
@@ -80,7 +80,7 @@ $db['default'] = array(
 	'password' => '123456',
 	'database' => 'dk_db',
 	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
+	'dbprefix' => 'dk',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
